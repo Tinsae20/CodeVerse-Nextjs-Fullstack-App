d66941d9ec08fb23aa5b3aab2649f2de3bfc4aad
Tinsae20 <tinsaeyirgu88@gmail.com>
2025-11-21 11:24:53 +0300

    uploading images using cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import Event from "@/database/event.model";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from 'cloudinary';
 
 export async function POST(req: NextRequest) {
     try {
@@ -17,6 +18,20 @@ export async function POST(req: NextRequest) {
         } catch (error) {
             return NextResponse.json({message: 'Invalid JSON data format!'}, {status: 400});
         }
+
+        const file = formData.get('image') as File;
+        if(!file) return NextResponse.json({message: 'Image file is required!'}, {status: 400})
+        
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        const uploadResult = await new Promise((resolve, reject)=> {
+            cloudinary.uploader.upload_stream({resource_type: 'image', folder: 'CodeVerse'}, (error, results)=> {
+                if(error) return reject(error);
+                resolve(results);
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string}).secure_url;
 
         const createdEvent = await Event.create(event);
 
